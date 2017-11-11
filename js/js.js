67a50d705dd907f1321c07f5a4709694979a51e1
brikogaming <brikogaming@gmail.com>
2017-11-11 22:13:29 +0100

    Easter Egg jebuany

--- js/js.js
+++ js/js.js
@@ -1,5 +1,7 @@
 var articleP;
 var size = 20;
+var key = [4];
+
 function sizedown(){
     if(size > 1){
         size--;
@@ -25,3 +27,22 @@ function sizeup(){
     articleP[i].style.fontSize = size+"px";
 }
 }
+
+document.addEventListener('keydown', function(event) {
+    if(event.keyCode == 67) {
+        key[0] = "c";
+    }
+    else if(event.keyCode == 72) {
+        key[1] = "h";
+    }
+    else if(event.keyCode == 85) {
+        key[2] = "u";
+    }
+    else if(event.keyCode == 74) {
+        key[3] = "j";
+    }
+    if(key[0] == "c"&&key[1] == "h"&&key[2] == "u"&&key[3] == "j"){
+        document.querySelector("#catzen").style.display = "block";
+    }
+});
+
